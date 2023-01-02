952dcaa6be0ea72469f5b032337cee18b4693cb2
rafjurek <rafal.jurek@ttpsc.pl>
2023-01-02 20:08:35 +0100

    Project overhaul

---
+++ src/main/java/pl/net/brach/LabelPrint.java
@@ -0,0 +1,47 @@
+package pl.net.brach;
+
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.print.PageFormat;
+import java.awt.print.Printable;
+import java.util.ArrayList;
+
+public class LabelPrint implements Printable {
+    //Set the label dimensions in printer's properties to: 50 mm x 30 mm. Margins: 0 mm
+
+    private static final String FONT_NAME = "Courier New";
+    private static final int FONT_SIZE = 8;
+    static final float PRINT_PAGE_HEIGHT = 60;
+    static final float PRINT_PAGE_WIDTH = 80;
+
+    ArrayList<String> labelText;
+
+    public LabelPrint(ArrayList<String> labelText) {
+        this.labelText = labelText;
+    }
+
+    @Override
+    public int print(Graphics graphics, PageFormat pageFormat, int pageIndex) {
+        //One page only
+        if (pageIndex > 0) {
+            return NO_SUCH_PAGE;
+        }
+
+        Graphics2D g2d = (Graphics2D) graphics;
+        g2d.setFont(new Font(FONT_NAME, Font.PLAIN, FONT_SIZE));
+
+        int loopCount = 0;
+
+        String line = labelText.get(loopCount++);
+
+        while (loopCount != labelText.size()) {
+            g2d.drawString(line, 0, loopCount * FONT_SIZE);
+
+            line = labelText.get(loopCount);
+
+            loopCount++;
+        }
+        return PAGE_EXISTS;
+    }
+}
---
+++ src/main/java/pl/net/brach/SummaryController.java
@@ -0,0 +1,62 @@
+package pl.net.brach;
+
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.shape.Line;
+import javafx.stage.Stage;
+
+public class SummaryController implements Initializable {
+
+    private static final double SUMMARY_WINDOW_PREFERRED_HEIGHT = 310;
+    private static final double BOTTOM_GRID_LINE_POSITION_VERTICAL = 152.0;
+    private static final double BUTTON_CLOSE_POSITION_VERTICAL = 177.0;
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd-MM-yyyy", Locale.ENGLISH);
+
+    @FXML
+    private Button bClose;
+    public Label transactionTableNumber;
+    public Label transactionRateDate;
+    public Label transactionAmount;
+    public Label transactionRate;
+    public Label transactionCalculatedAmount;
+    public Label transactionAmountVATLabel;
+    public Label transactionAmountVAT;
+    public Line bottomGridLine;
+    public BorderPane summaryWindow;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) { }
+
+    @FXML
+    public void generateSummary(String[] params) {
+        transactionTableNumber.setText(params[1]);
+        transactionRateDate.setText(LocalDate.parse(params[2]).format(DATE_FORMAT));
+        transactionAmount.setText(params[0]);
+        transactionRate.setText(params[3]);
+        transactionCalculatedAmount.setText(params[4]);
+
+        if (params.length == 6) {
+            transactionAmountVAT.setText(params[5]);
+            transactionAmountVATLabel.setVisible(true);
+            transactionAmountVAT.setVisible(true);
+        } else {
+            bottomGridLine.setLayoutY(BOTTOM_GRID_LINE_POSITION_VERTICAL);
+            bClose.setLayoutY(BUTTON_CLOSE_POSITION_VERTICAL);
+            summaryWindow.setPrefHeight(SUMMARY_WINDOW_PREFERRED_HEIGHT);
+        }
+    }
+
+    @FXML
+    private void closeClicked() {
+        Stage stage = (Stage) bClose.getScene().getWindow();
+        stage.close();
+    }
+}
